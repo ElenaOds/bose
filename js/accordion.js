67638bb40f5d779cd30de92300a9823d0f944e7e
Elena Pedko <elpg.ods@gmail.com>
2024-09-30 16:12:31 +0300

    feedback section is completed

--- js/accordion.js
+++ js/accordion.js
@@ -2,8 +2,7 @@
     const faqItems = document.querySelectorAll('.faq-item');
 
     faqItems.forEach((item) => {
-        const question = item.querySelector('.faq-question');
-        const answer = item.querySelector('.faq-text');
+        const question = item.querySelector('.faq-item-wrapper');
     
         question.addEventListener('click', () => {
             item.classList.toggle('open');
